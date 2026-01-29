f3b64f14313198803d8f1cc7112fda8a465a268f
maddev <141483538+roywalk3r@users.noreply.github.com>
2026-01-29 08:00:22 -0500

    Change route from '/m3u8-proxy' to '/ani-m3u8-proxy'

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,6 @@ import { Router } from "oak/mod.ts";
 import { m3u8Proxy } from "../utils/m3u8-proxy.js";
 
 const router = new Router();
-router.get("/m3u8-proxy", m3u8Proxy);
+router.get("/ani-m3u8-proxy", m3u8Proxy);
 
 export { router };
